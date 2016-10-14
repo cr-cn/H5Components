871187e9c6496a6d0235909c9e6acac999b86d0f
ray1208 <80959858@qq.com>
2016-10-15 00:29:47 +0800

    H5对象的开发完结

--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -4,8 +4,8 @@ var H5ComponentBase = function(name, cfg) {
     var id = ('h5_c_' + Math.random()).replace('.', '_');
 
     // 把当前的组件类型添加到样式中进行标记
-    var cls = 'h5_component_name_' + name + ' h5_component_' + cfg.type;
-    var component = $('<div class="h5_component ' + cls + '" id="' + id + '"></div>');
+    var cls = ' h5_component_' + cfg.type;
+    var component = $('<div class="h5_component' + cls + ' h5_component_name_' + name + '" id="' + id + '"></div>');
     cfg.text && component.text(cfg.text);
     // 考虑到iphone的屏幕关系
     cfg.width && component.width(cfg.width / 2);
