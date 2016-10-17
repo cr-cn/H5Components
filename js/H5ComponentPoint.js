6e9b65231665b5493308563004e983b66b748016
ray1208 <80959858@qq.com>
2016-10-17 15:58:12 +0800

    散点图完结

--- js/H5ComponentPoint.js
+++ js/H5ComponentPoint.js
@@ -5,7 +5,12 @@ var H5ComponentPoint = function(name, cfg) {
     $.each(cfg.data, function(index, item) {
         var point = $('<div class="point point_' + index + '">');
 
-        point.text(item[0] + '-' + item[1]);
+        var name = $('<div class="name">' + item[0] + '</div>')
+        var rate = $('<div class="per">' + (item[1] * 100) + '%</div>');
+
+        name.append(rate);
+        point.append(name);
+
         var per = (item[1] / base * 100) + '%';
         // console.log(per);
 
