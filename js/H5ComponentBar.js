aff44aac0cc772f7346f156c3faf1988137e1c30
ray1208 <80959858@qq.com>
2016-10-18 11:02:14 +0800

    柱状图组件，一次完成！

---
+++ js/H5ComponentBar.js
@@ -0,0 +1,30 @@
+// 柱状图表组件对象
+var H5ComponentBar = function(name, cfg) {
+    var component = new H5ComponentBase(name, cfg);
+
+    $.each(cfg.data, function(index, item) {
+        console.log(item);
+
+        var line = $('<div class="line">');
+        var name = $('<div class="name">');
+        var rate = $('<div class="rate">');
+        var per = $('<div class="per">');
+
+        var bgStyle = '';
+        if (item[2]) {
+            bgStyle = 'style="background-color:' + item[2] + '"';
+        }
+
+        var width = item[1] * 100 + '%';
+        rate.html('<div class="bg" ' + bgStyle + '></div>');
+        rate.css('width', width);
+
+
+
+        per.text(width);
+        name.text(item[0]);
+        line.append(name).append(rate).append(per);
+        component.append(line);
+    })
+    return component;
+}
