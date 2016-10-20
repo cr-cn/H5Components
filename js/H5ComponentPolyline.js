988e292954c7a1d547acf56156d159e7bb1817df
ray1208 <80959858@qq.com>
2016-10-20 11:19:10 +0800

    折线图组件开发完毕

--- js/H5ComponentPolyline.js
+++ js/H5ComponentPolyline.js
@@ -27,10 +27,17 @@ var H5ComponentPolyline = function(name, cfg) {
     }
     //垂直网格线（根据项目的个数去分）
     step = cfg.data.length + 1;
+    var text_w = w / step >> 0;
     for (var i = 0; i < step + 1; i++) {
         var x = (w / step) * i;
         ctx.moveTo(x, 0);
         ctx.lineTo(x, h);
+        if (cfg.data[i]) {
+            var text = $('<div class="text">');
+            text.text(cfg.data[i][0]);
+            text.css('width', text_w / 2).css('left', x / 2 - text_w / 4 + text_w / 2);
+            component.append(text);
+        }
     }
 
     ctx.stroke();
@@ -44,51 +51,94 @@ var H5ComponentPolyline = function(name, cfg) {
     cns.height = ctx.height = h;
     component.append(cns);
 
+    // 绘制折线以及对应的数据和阴影
+    // per:0到1之间的数据，会根据这个值绘制最终数据数据对应的中间状态
+    var draw = function(per) {
+        // 清空画布
+        ctx.clearRect(0, 0, w, h);
+        // 绘制折线数据
+        ctx.beginPath();
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#ff8878';
 
-    // 绘制折线数据
-    ctx.beginPath();
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = '#ff8878';
+        var x = 0;
+        var y = 0;
+        // ctx.moveTo(10, 10);
+        // ctx.arc(10, 10, 5, 0, 2 * Math.PI);
+        // step = cfg.data.length + 1;
+        // 
+        var row_w = (w / (cfg.data.length + 1));
+        //画点
+        for (i in cfg.data) { //这里的i就是这个cfg.data数组的下标0,1,2,3...
+            var item = cfg.data[i];
+            x = row_w * i + row_w; //为什么不能写成(i+1)
+            y = h * (1 - item[1] * per);
+            ctx.moveTo(x, y)
+            ctx.arc(x, y, 5, 0, 2 * Math.PI);
+        }
 
-    var x = 0;
-    var y = 0;
-    // ctx.moveTo(10, 10);
-    // ctx.arc(10, 10, 5, 0, 2 * Math.PI);
-    // step = cfg.data.length + 1;
-    // 
-    var row_w = (w / (cfg.data.length + 1));
-    //画点
-    for (i in cfg.data) { //这里的i就是这个cfg.data数组的下标0,1,2,3...
-        var item = cfg.data[i];
-        x = row_w * i + row_w; //为什么不能写成(i+1)
-        y = h * (1 - item[1]);
-        ctx.moveTo(x, y)
-        ctx.arc(x, y, 5, 0, 2 * Math.PI);
+        //连线
+        //移动画笔到第一个数据的点位置
+        ctx.moveTo(row_w, h * (1 - cfg.data[0][1] * per));
+        // 画第一个点
+        // ctx.arc(row_w, h * (1 - cfg.data[0][1]), 20, 0, 2 * Math.PI);
+        for (i in cfg.data) {
+            var item = cfg.data[i];
+            x = row_w * i + row_w;
+            y = h * (1 - item[1] * per);
+            ctx.lineTo(x, y);
+        }
+
+        ctx.stroke();
+
+        // ctx.lineWidth = 1;
+        ctx.strokeStyle = 'rgba(255, 255, 255, 0)';
+
+
+
+        //绘制阴影
+        ctx.lineTo(x, h);
+        ctx.lineTo(row_w, h);
+        ctx.fillStyle = 'rgba(255, 136, 120, 0.2)';
+        ctx.fill();
+
+
+        //写数据
+        for (i in cfg.data) {
+            var item = cfg.data[i];
+            x = row_w * i + row_w;
+            y = h * (1 - item[1] * per);
+
+            ctx.fillStyle = item[2] ? item[2] : '#595959';
+            ctx.fillText(((item[1] * 100) >> 0) + '%', x - 10, y - 10); //>>0用法
+        }
+
+
+        ctx.stroke();
     }
 
-    //连线
-    //移动画笔到第一个数据的点位置
-    ctx.moveTo(row_w, h * (1 - cfg.data[0][1]));
-    // 画第一个点
-    // ctx.arc(row_w, h * (1 - cfg.data[0][1]), 20, 0, 2 * Math.PI);
-    for (i in cfg.data) {
-        var item = cfg.data[i];
-        x = row_w * i + row_w;
-        y = h * (1 - item[1]);
-        ctx.lineTo(x, y);
-    }
+    //折线图的生长动画
+    component.on('onLoad', function() {
+        var s = 0;
+        for (var i = 0; i < 100; i++) {
+            setTimeout(function() { //闭包
+                    s += .01;
+                    draw(s);
+                }, i * 10 + 500) //i*10 思路
+        }
+    })
 
-    //写数据
-    for (i in cfg.data) {
-        var item = cfg.data[i];
-        x = row_w * i + row_w;
-        y = h * (1 - item[1]);
 
-        ctx.fillStyle = item[2] ? item[2] : '#595959';
-        ctx.fillText(((item[1] * 100) >> 0) + '%', x - 10, y - 10); //>>0用法
-    }
-
-    ctx.stroke();
+    //折线图的退场动画
+    component.on('onLeave', function() {
+        var s = 1;
+        for (var i = 0; i < 100; i++) {
+            setTimeout(function() {
+                s -= .01;
+                draw(s);
+            }, i * 10)
+        }
+    })
 
     return component;
 }
